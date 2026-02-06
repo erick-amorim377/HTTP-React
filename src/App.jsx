2caead97f7c5a3a48860b09d82204db737a2092f
erick-amorim377 <erickamorim221@gmail.com>
2026-02-06 14:30:30 -0300

    Carregamento de dados dinâmicos

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,11 @@ function App() {
       },
       body: JSON.stringify(product),
     });
+    const addedProduct = await response.json()
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+    setName("");
+    setPrice("");
    }
 
   return (
